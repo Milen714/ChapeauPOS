21e34b00ff757e65e586a848200a5f5684d6cab1
Milen714 <milensavov1997@gmail.com>
2025-04-15 20:20:25 +0200

    dasd

---
+++ Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ChapeauPOS.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+        public LoginModel(string username, string password, bool rememberMe)
+        {
+            UserName = username;
+            Password = password;
+            RememberMe = rememberMe;
+        }
+
+        public LoginModel()
+        {
+        }
+    }
+}
