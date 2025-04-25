4903e7b928d9337af8c9d0a365170234e1363cf7
Milen714 <milensavov1997@gamil.com>
2025-04-25 02:05:43 +0200

    Some Tables

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -14,11 +14,16 @@ namespace ChapeauPOS.Controllers
         //  testing data
         private static List<Table> Tables = new List<Table>
         {
-            new Table(1, 4, 6, TableStatus.Free),
-            new Table(2, 2, 4, TableStatus.Free),
-            new Table(3, 6, 4,  TableStatus.Free),
-            new Table(4, 8, 8, TableStatus.Free),
-            new Table(5, 10, 2, TableStatus.Free)
+            new Table(1, 1, 6, TableStatus.Free),
+            new Table(2, 2, 4, TableStatus.Reserved),
+            new Table(3, 3, 4,  TableStatus.Occupied),
+            new Table(4, 4, 8, TableStatus.Reserved),
+            new Table(5, 5, 4, TableStatus.Free),
+            new Table(6, 6, 2, TableStatus.Free),
+            new Table(7, 7, 2, TableStatus.Occupied),
+            new Table(8, 8, 2, TableStatus.Free),
+            new Table(9, 9, 2, TableStatus.Occupied),
+            new Table(10, 10, 2, TableStatus.Occupied)
         };
         public IActionResult Index()
         {
