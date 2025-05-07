6527e8f292870266736628949e103cc89b84ad97
Milen714 <milensavov1997@gamil.com>
2025-05-08 01:36:42 +0200

    Some table,order services and repos

---
+++ Repositories/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using ChapeauPOS.Models;
+
+namespace ChapeauPOS.Repositories.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        List<Order> GetAllOrders();
+        Order GetOrderById(int orderId);
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(int orderId);
+        List<Order> GetOrdersByTableId(int tableId);
+        List<Order> GetOrdersByEmployeeId(int employeeId);
+        List<Order> GetOrdersByStatus(OrderStatus status);
+    }
+}
