da445637097ef2ad6c1036d2da5a15b6c9884feb
bishwajit-Dhar <bdhar3113@gmail.com>
2025-05-19 13:23:10 +0200

    Add

--- Repositories/Interfaces/IMenuRepository.cs
+++ Repositories/Interfaces/IMenuRepository.cs
@@ -4,11 +4,13 @@ namespace ChapeauPOS.Repositories.Interfaces
 {
     public interface IMenuRepository
     {
-        List<MenuItem> GetAllMenuItems();
+        List<MenuItem> GetAllMenuItems(bool includeInactive=false);
+        List<MenuItem> FilterMenuItems(string course, string category);
         MenuItem GetMenuItemById(int id);
         void AddMenuItem(MenuItem menuItem);
         void UpdateMenuItem(MenuItem menuItem);
         void DeleteMenuItem(int id);
+        void ToggleMenuItemStatus(int id, bool isActive);
         List<MenuCategory> GetMenuCategories();
         List<MenuItem> GetMenuItemsByCategory(MenuCategory category);
         List<MenuItem> GetMenuItemsByCourse(MenuCourse course);
