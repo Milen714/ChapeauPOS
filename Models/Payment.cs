0511d5ce4efd55febf0c1cefa14d4ddddc804022
Nishchal-code <159100682+Nishchal-code@users.noreply.github.com>
2025-05-22 22:43:38 +0200

    .

---
+++ Models/Payment.cs
@@ -0,0 +1,41 @@
+﻿namespace ChapeauPOS.Models
+{
+    public class Payment
+    {
+        public int PaytmentID { get; set; }
+        public int BillID { get; set; }
+        
+
+        public PaymentMethod PaymentMethod { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime PaidAt { get; set; }
+        public decimal TipAmount { get; set; }
+        public decimal LowVAT { get; set; }
+        public decimal HighVAT { get; set; }    
+        public string FeedBack { get; set; }
+
+        public Payment()
+        {
+            
+        }
+
+        public Payment(int paytmentID, int billID, PaymentMethod paymentMethod, decimal totalAmount, DateTime paidAt, decimal tipAmount, decimal lowVAT, decimal highVAT, string feedBack)
+        {
+            PaytmentID = paytmentID;
+            BillID = billID;
+            PaymentMethod = paymentMethod;
+            TotalAmount = totalAmount;
+            PaidAt = paidAt;
+            TipAmount = tipAmount;
+            LowVAT = lowVAT;
+            HighVAT = highVAT;
+            FeedBack = feedBack;
+        }
+    }
+    public enum PaymentMethod
+    {
+        Cash,
+        Maestro,
+        Voucher
+    }
+}
