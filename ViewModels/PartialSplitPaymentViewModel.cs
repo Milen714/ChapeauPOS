dd30e948eb637c886e42c60369bade20d2811ad9
Nishchal-code <159100682+Nishchal-code@users.noreply.github.com>
2025-06-11 21:59:40 +0200

    Partial Payment

---
+++ ViewModels/PartialSplitPaymentViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using ChapeauPOS.Models;
+
+namespace ChapeauPOS.ViewModels
+{
+    public class PartialPaymentViewModel
+    {
+        public int TableId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<EqualIndividualPayment> Payments { get; set; }= new List<EqualIndividualPayment>();
+        public PartialPaymentViewModel()
+        {
+            
+        }
+    }
+
+}
